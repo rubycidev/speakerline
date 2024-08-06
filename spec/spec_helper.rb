52758761627517ee47e682528c17f7b4a8800dc7
rubycidev <117899216+rubycidev@users.noreply.github.com>
2024-08-06 14:51:35 +0200

    trigger new build with gitlab_builds

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,20 +1,5 @@
 require 'simplecov'
-# if ENV["RUBY_CI_SECRET_KEY"]
-#   require "rspec/core/runner"
-#   require "ruby_ci/runner_prepend"
 
-#   class RSpec::Core::ExampleGroup
-#     def self.filtered_examples
-#       rubyci_scoped_ids = Thread.current[:rubyci_scoped_ids] || ""
-
-#       RSpec.world.filtered_examples[self].filter do |ex|
-#         rubyci_scoped_ids == "" || /^#{rubyci_scoped_ids}($|:)/.match?(ex.metadata[:scoped_id])
-#       end
-#     end
-#   end
-
-#   RSpec::Core::Runner.prepend(RubyCI::RunnerPrepend)
-# end
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
